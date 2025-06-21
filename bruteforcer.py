3bb2ec36881ef90c8be2c286b4264c0bbf439f51
j3r1ch0123 <110147529+j3r1ch0123@users.noreply.github.com>
2025-06-20 22:12:33 -0400

    Update bruteforcer.py

--- bruteforcer.py
+++ bruteforcer.py
@@ -50,10 +50,7 @@ def main():
     parser.add_argument("--verbose", action="store_true", help="Enable status updates during brute force")
     
     args = parser.parse_args()
-    if args.help:
-        parser.print_help()
-        exit(0)
-
+    
     print(f"[*] Target URL: {args.url}")
     print(f"[*] Using email file: {args.email_file}")
     print(f"[*] Digits: {args.min_digits} to {args.max_digits}")
